f28f489d68407cb1edeaa709509aeb8acc8c535d
nishizawayuya <22aw0128@jec.ac.jp>
2023-05-23 10:26:25 +0900

    verhenkou

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+
 function DetailsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
